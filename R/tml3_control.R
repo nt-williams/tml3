4e686495b2bad97882f2467ff9f8a46db2eb085d
Nicholas Williams <ntwilliams.personal@gmail.com>
2024-09-11 09:15:58 -0700

    basic control params

--- R/tml3_control.R
+++ R/tml3_control.R
@@ -1,3 +1,11 @@
-tml3_control <- function() {
-
+tml3_control <- function(.trt_folds = NULL,
+                         .outcome_folds = NULL,
+                         .cens_folds = NULL,
+                         .trim = 0.999,
+                         .discrete = TRUE) {
+  list(.discrete = .discrete,
+       .trim = .trim,
+       .outcome_folds = outcome_folds,
+       .trt_folds = trt_folds,
+       .cens_folds = cens_folds)
 }
